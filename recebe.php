cad1cc972f9fabeba9d6f48e7a328562c4085802
Priscila Pradella <priscila_pradella@estudante.sc.senai.br>
2019-08-30 16:31:21 -0300

    Cadastro usuário
    closes#8

--- recebe.php
+++ recebe.php
@@ -11,33 +11,43 @@ function verificar_entrada($entrada){
 
 if(isset($_POST['action']) && $_POST['action'] == 'cadastro'){
         $nomeCompleto = verificar_entrada($_POST['nomeCompleto']);
-        $nomeUsuário = verificar_entrada($_POST['nomeUsuário']);
-        $emailUsuário = verificar_entrada($_POST['emailUsuário']);
-        $senhaUsuário = verificar_entrada($_POST['senhaUsuário']);
+        $nomeUsuario = verificar_entrada($_POST['nomeUsuário']);
+        $emailUsuario = verificar_entrada($_POST['emailUsuário']);
+        $senhaUsuario = verificar_entrada($_POST['senhaUsuário']);
         $senhaConfirma = verificar_entrada($_POST['senhaConfirma']);
         $concordar = $_POST['concordar'];
         $dataCriacao = date("Y-m-d H:i:s");
 
-        $senha = sha1($senhaUsuário);
+        $senha = sha1($senhaUsuario);
         $senhaC = sha1($senhaConfirma);
-
-
-        echo "<h5>Nome completo: $nomeCompleto</h5>";
-        echo "<h5>Nome Usuário: $nomeUsuário</h5>";
-        echo "<h5>E-mail Usuário: $emailUsuário </h5>";
-        echo "<h5>Senha Usuário: $senhaUsuário</h5>";
-        echo "<h5>Senha Confirma: $senhaConfirma</h5>";
-        echo "<h5>Concorda: $concordar</h5>";
-        echo "<h5>Data de Criação: $dataCriacao</h5>";
 
         if($senha != $senhaC){
             echo "<h1>As senhas não conferem</h1>";
             exit();
         }else{
-            echo "<h5> senha codificada: $senha</h5>";
-            
+            $sql = $conecta->prepare("SELECT nomeUsuario, senha FROM
+            usuario WHERE nomeUsuario = ? OR email = ?");
+            $sql->bind_param("ss",$nomeUsuario, $emailUsuario);
+            $sql->execute();
+            $resultado = $sql->get_result();
+            $linha = $resultado->fetch_array(MYSQLI_ASSOC);
+            if($linha['nomeUsuario'] == $nomeUsuario){
+                echo "<p>Nome de usuário indisponível, tente outro.</p>";
+            }elseif($linha['email'] == $nomeUsuario){
+                echo "<p>E-mail já em uso, tente outro</p>";
+            }else{
+                $sql = $conecta->prepare("INSERT into usuario
+                (nome, nomeUsuario, email, senha, dataCriacao) values(?,?,?,?,?)");
+                $sql->bind_param("sssss",$nomeCompleto, $nomeUsuario,
+                $emailUsuario, $senha, $dataCriacao);
+                if($sql->execute()){
+                    echo "<p>Registro com sucesso</p>";
+                }else{
+                    echo"<p>Algo deu errado. Tente outra vez.</p>";
+                }
+            } 
         }
-}else{
+    }else{
     echo "<h1 style='color:red'>
     Esta página não pode ser acessada diretamente</h1>";
 
